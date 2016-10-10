7601ee61ac1a0a006632e33cda30032e96675f0a
Gunnar Bjarki Bjornsson <gunnar@stokkur.is>
2016-10-10 22:46:54 +0000

    refactoring test multiple numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,19 +3,27 @@ package is.ru.stringcalculator;
 public class Calculator{
 
   public static int add(String text){
+
     if (text.equals("")) {
       return 0;
     }
     else if (text.contains(",")){
-      String [] numbers = text.split(",");
-      int total = 0;
-      for (String number: numbers ) {
-        total += toInt(number);
-      }
-      return total;
+      return sumOfMultipleNumbers(splitNumbers(text));
     }
     return 1;
   }
+
+  public static String[] splitNumbers(String text){
+    return text.split(",");
+  }
+
+  public static int sumOfMultipleNumbers(String [] numbers){
+    int total = 0;
+       for(String number : numbers){
+       total += toInt(number);
+   }
+   return total;
+}
 
   public static int toInt(String text){
     return Integer.parseInt(text);
